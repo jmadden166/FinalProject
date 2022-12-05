c75b27c990a2711313705964c9351ac71d0099da
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-05 13:44:48 -0600

    Update drivers.php

--- drivers.php
+++ drivers.php
@@ -8,6 +8,19 @@
 </head>
     <body>
     <h1>Current Driver Lineup</h1>
+         <div class="dropdown">
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+        </a>
+
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="drivers.php">Current Drivers</a></li>
+            <li><a class="dropdown-item" href="Past.html">Race Results</a></li>
+            <li><a class="dropdown-item" href="races.html">Upcoming Races</a></li>
+            <li><a class="dropdown-item" href="Standings.html">Current Standings</a></li>
+          <li><a class="dropdown-item" href="admin.php">Admin Window</a></li>
+        </ul>
+    </div>
 <?php include 'drivers2.php'; ?>
 <?php include 'insert2.php'; ?>      
   </body>
