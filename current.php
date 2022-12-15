82e2522f97aeecdace34dd5271550acb38b09734
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-15 17:49:48 -0600

    Update current.php

--- current.php
+++ current.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Drivers for Team for the 2022 Season</h1>
+
+
+<form action="/action.php">
+  <label for="Team">Choose a Team:</label>
+  <select name="Team" id="Team">
+    <option value="Red Bull Racing">Red Bull Racing</option>
+    <option value="Ferrari">Ferrari</option>
+    <option value="Mercedes">Mercedes</option>
+    <option value="McLaren">McLaren</option>
+    <option value="Alpine">Alpine</option>
+    <option value="Aston Martin">Aston Martin</option>
+    <option value="AlphaTauri">AlphaTauri</option>
+    <option value="Alfa Romeo">McLaren</option>
+    <option value="Haas">Haas</option>
+    <option value="Williams">Williams</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
+
+<p>Click the "Submit" button and get the Drivers for that Team</p>
+
+</body>
+</html>
 
