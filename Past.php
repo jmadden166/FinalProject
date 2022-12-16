bda54c45b203bc0b13ff587fcfbfcd0a0c1cce69
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-16 00:14:27 -0600

    Create Past.php

---
+++ Past.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Race Results</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+    <style>
+            h1 {
+                text-align: center;
+            }
+body {
+  background-color: #f18973;
+}
+
+  </style>
+    <h1>Upcoming Races</h1>
+<a href="index.html">Return to the Home Page</a> 
+  <br>
+  <center>
+  <body>
+<?php include 'past2.php'; ?> 
+  </body>
+  </center>
+  </html>
