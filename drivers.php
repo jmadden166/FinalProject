7908f4533b160ef6ee129ac1597685fae497d97d
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-15 18:01:13 -0600

    Update drivers.php

--- drivers.php
+++ drivers.php
@@ -6,21 +6,13 @@
     <title>2022 Race Winners</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
-    
+    <style>
+            h1 {
+                text-align: center;
+            }
+  </style>
     <h1>Current Driver Lineup</h1>
-         <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Menu
-        </a>
-
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="drivers.php">Current Drivers</a></li>
-            <li><a class="dropdown-item" href="Past.html">Race Results</a></li>
-            <li><a class="dropdown-item" href="races.html">Upcoming Races</a></li>
-            <li><a class="dropdown-item" href="Standings.html">Current Standings</a></li>
-          <li><a class="dropdown-item" href="admin.php">Admin Window</a></li>
-        </ul>
-    </div>
+<a href="index.html">Return to the Home Page</a> 
   <body>
 <?php include 'drivers2.php'; ?>
 <?php include 'insert2.php'; ?>      
