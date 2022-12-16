66f90cdd989dfa8c9b8c99b9713d834ffad3e28f
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-15 23:03:08 -0600

    Update action.php

--- action.php
+++ action.php
@@ -3,14 +3,7 @@
    <left>
         <a href="index.html">Return to the Home Page</a> 
      </left>
-   <style>
-   img {
-    float: left;
-    width:  100px;
-    height: 400px;
-    object-fit: cover;
-}
-        </style>
+
   <?php
   $servername = "localhost";
 $username = "jackstra_Homework3";
