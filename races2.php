a9805a3157ab14a14e77894cab950e28c3b2d48e
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-12-15 23:56:52 -0600

    Create races2.php

---
+++ races2.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <?php
+  $servername = "localhost";
+$username = "jackstra_Homework3";
+$password = "+JMx3n7)k+!H";
+$dbname = "jackstra_final";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  ?>
+  <?php  
+ 
+$sql = "SELECT RaceName, Date From 2022Races join RaceDate on 2022Races.RaceID = RaceDate.RaceID WHERE Date > DATE(NOW()) OR (Date = DATE(NOW()) AND Date > TIME(NOW()) ) ORDER BY Date ASC, Date ASC ";
+$result2 = $conn->query($sql);
+
+if ($result2->num_rows > 0) {
+  // output data of each row
+  while($row = $result2->fetch_assoc()) {
+    ?>
+     <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["Race Name"]?></h5>
+      <p class="card-text"><ul>
+<?php
+  
+    echo "<li>Date: " . $row["Date"]. "</li>";
+   ?>
+      <br>
+         </ul></p>
+  </div>
+    </div>
+  <?php  
+  }
+} else {
+  echo "There are no more upcoming races";
+}
+$conn->close();
+?>
+</html>
